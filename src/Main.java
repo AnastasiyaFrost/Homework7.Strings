e3854cf283b34c9ce159467d0955a8da0e7d74e4
Анастасия <grebneva.nastena@yandex.ru>
2022-08-31 11:00:38 +0300

    First commit.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
+    public static String main(String[] args) {
+        //Task 1
+        String firstName = "Иван ";
+        String middleName = "Иванович ";
+        String lastName = "Иванов ";
+        String fullName = firstName+middleName+lastName;
+        System.out.println("ФИО сотрудника: " + fullName);
 
-        System.out.println("Hello world!");
+        //Task 2
+
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника для документов: " + fullNameUpperCase);
+
+        return firstName;
     }
 }
